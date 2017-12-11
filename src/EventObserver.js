4b47ef8a0f8690f008e8c8f20a9c87b0fc33c3f3
Rostislav Rakaev <rostislav.rakaev@gmail.com>
2017-12-11 07:40:06 +0200

    EventObserver to connect Models -> UI

--- src/EventObserver.js
+++ src/EventObserver.js
@@ -1,27 +1,40 @@
 class EventObserver {
+
   constructor() {
+
     this.observers = [];
     this.on = this.on.bind(this);
   }
 
 on(event, fn) {
-	// console.log('SUBSCRIBED', event, fn);
+
   this.observers.push({ evt: event, fn: fn});
 }
 
+off(event) {
+
+  this.observers = this.observers.filter((observer) => observer.evt === event);
+}
+
+unbindAll() {
+
+  this.observers = [];
+}
+
 unbind(fn) {
-  this.observers = this.observers.filter((subscriber) => subscriber !== fn);
+
+  this.observers = this.observers.filter((subscriber, i) => {
+    return  subscriber.fn !== fn
+  });
 }
 
 trigger(event, data) {
-	// console.log('observers', this.observers);
-	// console.log('TRIGGER', event, data);
+
   this.observers.filter((observer) => {
   	
   	return observer.evt === event;
   }).forEach((subscriber) => {
-    // console.log('filtered', subscriber);
-    subscriber.fn(data)
+    subscriber.fn(data);
   });
 }
 }
